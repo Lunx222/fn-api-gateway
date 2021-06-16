623c9bc1c26f42590e72a2aff24b96b10dbb9e47
Seraphina <346730305@qq.com>
2021-06-16 14:18:13 +0800

    Initialize api gateway.

---
+++ src/main/java/com/fn/fnapigateway/FnApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.fn.fnapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FnApiGatewayApplication.class, args);
+	}
+
+}
